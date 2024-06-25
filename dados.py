e3d6f3e506c361a8af216e4aa66599144a77ffd3
mthPiltz <matheus.piltz@ufms.br>
2024-06-24 22:27:00 -0400

    first commit

---
+++ dados.py
@@ -0,0 +1,39 @@
+from sklearn.datasets import fetch_covtype as fc
+from sklearn.datasets import fetch_california_housing
+from sklearn.datasets import fetch_rcv1
+from sklearn import datasets
+import pandas as pd
+
+
+# cov_type = fc()
+# print(cov_type.data)  #dados de topologia de florestar
+
+# data = datasets.load_breast_cancer() #dados de pacientes com cancer
+# print(data.feature_names)
+
+# data = datasets.load_diabetes() #dados de pacientes com diabetes
+# print(data.feature_names)
+
+# df = pd.read_csv(r"datasets\04 - Customer Churn Prediction\customer_churn_data.csv")
+# print(df.head()) #dados sobre rotatividade de clientes| target churn
+
+# data = datasets.load_digits() #dados de digitos escritos
+# print(data.feature_names)
+
+# data = datasets.load_iris() #dados de flores iris
+# print(data.feature_names)
+
+# data = datasets.load_wine() #dados sobre vinhos
+# print(data.feature_names)
+
+# housing = fetch_california_housing() # dados sobre a ocupacao da californa
+# print(housing.feature_names, housing.target)
+
+# df = pd.read_csv(r"datasets\weather_classification_data\weather_classification_data.csv")
+# print(df.head()) # dados artificiais do tipo de clima| target Type
+
+# df = pd.read_csv(r"datasets\Student_performance_data _\Student_performance_data _.csv")
+# print(df.head()) # dados artificiais do desempenho de estudantes| target GradeClass
+
+# df = pd.read_csv(r"datasets\air_quality_health_impact_data\air_quality_health_impact_data.csv")
+# print(df.head()) # dados sobre a qualidade do ar e impacto na saude publica| target HealthImpactClass
